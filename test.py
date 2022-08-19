396c4642603c34c50f86f3ba28af4c09da4a0895
Ojas Surana <code@ojassurana.com>
2022-08-19 17:30:17 +0800

    Init

--- test.py
+++
@@ -1,7 +0,0 @@
-import pyrogram
-
-
-ojas = pyrogram.Client("xue_en587", api_id=16480543, api_hash="5d3cffde3f291638eaa9a2b6205875ef")
-ojas.start()
-chat = ojas.phone_number("+919765059193")
-print(chat.id)
